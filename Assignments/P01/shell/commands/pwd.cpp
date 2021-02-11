96c65279cecbc8df960f57054f3fe02f0f4f4fbe
gayalhk <35520396+gayalhk@users.noreply.github.com>
2021-02-11 16:43:06 -0600

    Create pwd.cpp

---
+++ Assignments/P01/shell/commands/pwd.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#ifdef WINDOWS
+#include <direct.h>
+#define GetCurrentDir _getcwd
+#else
+#include <unistd.h>
+#define GetCurrentDir getcwd
+#endif
+#include<iostream>
+ 
+std::string GetCurrentWorkingDir( void ) {
+  char buff[FILENAME_MAX];
+  GetCurrentDir( buff, FILENAME_MAX );
+  std::string current_working_dir(buff);
+  return current_working_dir;
+}
+ 
+int main(){
+  std::cout << GetCurrentWorkingDir() << std::endl;
+  return 1;
+}
